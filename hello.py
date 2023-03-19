b0e146ec3adfedad0525a0294642f408cf432d3a
mf009 <misha.frol@gmail.com>
2023-03-19 19:52:41 +0300

    новый hello в main

--- hello.py
+++ hello.py
@@ -1,21 +1,3 @@
 # hello.py
 
 
-print ("------------------languadge-----------------")
-num = int(input("Enter a num "))
-if num > 10 and num < 12:
-    print("ok ")
-else:
-    print("NO") 
-
-name = input ("Enter your name:  ")
-length = len (name)
-print (length)
-print (name.capitalize()) 
-print (name [7:10])
-
-
-//запись
-//запись2
-//Hello world
-//Новая строка
